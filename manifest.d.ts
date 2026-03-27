abdb8a02101b43527721b17c2acbbf19adaf1fcc
tokalahr <tokalahr@gmail.com>
2026-03-26 19:40:29 -0500

    setup

---
+++ manifest.d.ts
@@ -0,0 +1,4 @@
+declare module '*.json' {
+    const value: chrome.runtime.ManifestV3;
+    export default value;
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { crx } from '@crxjs/vite-plugin'
+import manifest from './manifest.config.ts'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), crx({ manifest })],
+  server: {
+    cors: {
+      origin: [
+        /chrome-extension:\/\//,
+      ],
+    },
+  },
+  
 })
