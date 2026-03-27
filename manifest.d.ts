65abcf8c5cba982696b19121f52540f82b0651dc
Obinna Anaenugwu <oeanaenugwu@gmail.com>
2026-03-26 19:45:52 -0500

    fixed error

--- manifest.d.ts
+++
@@ -1,4 +0,0 @@
-declare module '*.json' {
-    const value: chrome.runtime.ManifestV3;
-    export default value;
-}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { crx } from '@crxjs/vite-plugin'
-import manifest from './manifest.config.ts'
+import manifest from './manifest.config.js'
 
 // https://vite.dev/config/
 export default defineConfig({
